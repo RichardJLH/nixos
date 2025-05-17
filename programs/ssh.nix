7a49441c60d7c45e87b83329cc271cb64cdfc21b
Richard <richard.network96@gmail.com>
2025-05-17 19:39:08 +0200

    simplify ssh keys to one

--- programs/ssh.nix
+++ programs/ssh.nix
@@ -4,28 +4,6 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    matchBlocks =
-	  let
-	    privateIdentity = "${config.home.homeDirectory}/.ssh/id_ed25519_pri";
-	    publicIdentity = "${config.home.homeDirectory}/.ssh/id_ed25519_pro";
-	  in
-	  {
-        "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = privateIdentity;
-        };
-        "github.com-work" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = publicIdentity;
-        };
-        "git.tu-berlin.de" = {
-          hostname = "git.tu-berlin.de";
-          user = "git";
-          identityFile = publicIdentity;
-        };
-      };
   };
   services.ssh-agent.enable = true;
 }
